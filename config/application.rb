6c23b72bdb93e28e93056881ab9d1241cc748d16
Chris Alley <chris@chrisalley.info>
2013-08-11 14:57:47 +1200

    Load application.yml

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
+require 'yaml'
+APP_CONFIG = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
